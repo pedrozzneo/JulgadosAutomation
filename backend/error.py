5fb36308de56c451435a10671e91a6103caa2b0a
pedro <pedrozzneo@gmail.com>
2025-05-28 15:20:33 -0300

    smt was missing

--- backend/error.py
+++ backend/error.py
@@ -1,3 +1,5 @@
+import app
+
 error_log = []
 
 def log(classe, date, context):
@@ -14,3 +16,26 @@ def display():
         for i, error in enumerate(error_log, start=1):
             print(f"{i}- {error}")
 
+def solve(driver, download_dir):
+    try:
+        # Display the error log
+        display()
+
+        # Give it 3 times the len of error log to solve the errors
+        for i in range(len(error_log) * 3):
+            # Display the error log 
+            display()
+            
+            # Remove the first item to try to solve it and also recycle the error log
+            entry = error_log.pop(0) 
+            
+            # Split to extract class and date
+            parts = entry.split(", ")
+            classe = parts[0].split(": ")[1]
+            date = parts[1].split(": ")[1]
+            print(f"Trying to solve: {classe} on {date}")
+
+            # Try to solve
+            app.scrape(driver, classe, date, download_dir)
+    except:
+       raise
